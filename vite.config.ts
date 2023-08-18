32729c4438eb37c32e204851b4154726c396055f
Alexander Juul <aju@washworld.dk>
2023-08-18 23:24:44 +0200

    Vite config base changed for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    // base: '/devChallenges-windbnb',
-    base: '/',
+    base: '/devChallenges-windbnb',
+    // base: '/',
     plugins: [react()],
     build: {
         outDir: 'build',
