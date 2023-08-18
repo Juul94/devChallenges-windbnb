c59880ea156089ecf2a639e7c9f2ea9ed528d779
Alexander Juul <aju@washworld.dk>
2023-08-18 23:12:01 +0200

    GH Pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/devChallenges-windbnb',
+    // base: '/devChallenges-windbnb',
+    base: '/',
     plugins: [react()],
     build: {
         outDir: 'build',
